60ccf0eba126905ba29338909d4840625e8d13b3
sunday00tdi <102128587+sunday00tdi@users.noreply.github.com>
2023-02-12 00:23:55 +0900

    :recycle: test
    test

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -3,9 +3,9 @@ import {RepoCode, projectsState} from "@s/main_store";
 import Section from "@c/HomeServices/Section";
 
 const Home = () => {
-  const threeProjects = useRecoilValue(projectsState(
-    {repoCode: RepoCode.THREE, page: 1}
-  ))
+  // const threeProjects = useRecoilValue(projectsState(
+  //   {repoCode: RepoCode.THREE, page: 1}
+  // ))
 
   return (
     <div className="home-wrap flex flex-col w-full border-opacity-50">
